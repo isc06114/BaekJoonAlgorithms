9ebf541688d0521da12c9b585ceb0196fb4ee323
isc06114 <88813412+isc06114@users.noreply.github.com>
2024-01-27 18:54:17 +0900

    [D3] Title: 이진수 표현, Time: 13 ms, Memory: 13,544 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
@@ -1,17 +1,18 @@
-
-#include<iostream>
-
+#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
 using namespace std;
-
 int main(int argc, char** argv)
 {
 	int T, M, N;
-	cin >> T;
+	scanf(" %d", &T);
 	for (int test_case = 1; test_case <= T; test_case++) {
-		cin >> N>> M;
-		if ((M & (1 << N) - 1) == ((1 << N) - 1))
-			printf("#%d ON\n",test_case);
-		else printf("#%d OFF\n",test_case);
+		scanf(" %d %d", &N,&M);
+		if ((M & (1 << N) - 1) == ((1 << N) - 1)) {
+			printf("#%d ON\n", test_case);
+		}
+		else {
+			printf("#%d OFF\n", test_case);
+		}
 	}
 
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
