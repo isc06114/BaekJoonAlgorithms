ada6d962df1e0b4fc856696aeb2c90cbf9ed2359
isc06114 <88813412+isc06114@users.noreply.github.com>
2024-01-31 17:15:04 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 820 ms, Memory: 114312 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -3,44 +3,50 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N,M, dp[][],arr[][];
+    static int N,M, dp[][];
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new int[N+1][N+1];
-        dp = new int[N+1][N+1];
-        for(int i = 1; i<=N;i++){
+        dp = new int[N][N];
+        for(int i = 0; i<N;i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 1; j<=N;j++) arr[i][j] = Integer.parseInt(st.nextToken());
+            for(int j = 0; j<N;j++) dp[i][j] = Integer.parseInt(st.nextToken());
         }
-        for(int i = 1; i<=N;i++) for(int j = 1; j<=N;j++)  dp[i][j]=dp[i-1][j]+dp[i][j-1]+arr[i][j]-dp[i-1][j-1];
+        for(int i=1; i<N; i++){
+            dp[0][i] += dp[0][i-1];
+            dp[i][0] += dp[i-1][0];
+        }
+        for(int i = 1; i<N;i++) for(int j = 1; j<N;j++)  dp[i][j]+=dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1];
         
-        int fx,fy,sx,sy;
+        int fx,fy,sx,sy,res;
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
-            sx= Integer.parseInt(st.nextToken())-1;
-            sy= Integer.parseInt(st.nextToken())-1;
-            fx=Integer.parseInt(st.nextToken());
-            fy=Integer.parseInt(st.nextToken());
+            sx= Integer.parseInt(st.nextToken())-2;
+            sy= Integer.parseInt(st.nextToken())-2;
+            fx=Integer.parseInt(st.nextToken())-1;
+            fy=Integer.parseInt(st.nextToken())-1;
             //System.out.println(dp[fx][fy]+", +"+dp[sx][sy]+", -"+dp[fx][sy]+", -"+dp[sx][fy]);
-            sb.append(dp[fx][fy]+dp[sx][sy]-dp[fx][sy]-dp[sx][fy]);
+            res = dp[fx][fy];
+            if(sx>=0) res-=dp[sx][fy];
+            if(sy>=0) res-=dp[fx][sy];
+            if(sx>=0&&sy>=0) res+=dp[sx][sy];
+            sb.append(res);
             sb.append('\n');
         }
         System.out.print(sb);
         
 
-        //debug(arr);
         //debug(dp);
         
     }
 
 
     static void debug(int[][] f_array){
-        for(int i = 1; i<=N;i++){
-            for(int j = 1; j<=N;j++){
+        for(int i = 0; i<N;i++){
+            for(int j = 0; j<N;j++){
                 System.out.print(f_array[i][j]+" ");
             }
             System.out.println();
