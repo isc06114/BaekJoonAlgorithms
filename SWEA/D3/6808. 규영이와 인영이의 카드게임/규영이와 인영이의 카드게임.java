a22e2543f72c855eafc15b824e6e343e192d0c11
isc06114 <88813412+isc06114@users.noreply.github.com>
2024-02-15 17:52:52 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 4,704 ms, Memory: 19,452 KB -BaekjoonHub

---
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -0,0 +1,101 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+
+//11,468 kb 340 ms
+public class Solution {
+    static int T;
+    static int[] computerSet = new int[9];
+    static int player, computer, cnt,win,lose;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        initFI();
+        sb = new StringBuilder();
+        T = nextInt();
+        for(int test_case = 1; test_case<=T; test_case++){
+            sb.append('#').append(test_case).append(' ');
+            TC();
+        }
+        System.out.println(sb);
+    }
+    static void TC() throws IOException{
+        initInput();
+        dfs(player,0,0);
+        sb.append(win).append(' ').append(lose).append('\n');
+    }
+
+    static void initInput()throws IOException{
+        cnt =0; win =0; lose = 0;
+        computer = 0;
+        for(int i =0; i<9; i++){
+            int b = nextInt();
+            computer+= 1<<(b-1);
+            computerSet[i] = b;
+        }
+        player = ((~computer)&((1<<18)-1));
+    }
+
+    static void dfs(int bitMask, int score, int cnt){
+        if(bitMask == 0||cnt==9){
+            if(score>0){
+                win++; 
+                //System.out.println("WIN");
+
+            }
+            else if(score<0){
+                lose++;
+                //System.out.println("Lose");
+            }
+            return;
+        }
+        int comb = bitMask;
+
+        while(comb>0){
+            int cardBit = (comb&-comb)&(1<<18)-1;
+            int card = (int)(Math.log(cardBit)/Math.log(2))+1;
+            //if(cnt == 0)System.out.println("cnt="+cnt+", comb="+comb+", cardBit="+cardBit+", card="+card+", computerCard="+computerSet[cnt]+", score:"+score);
+            int temp =card-computerSet[cnt];
+            dfs(bitMask - cardBit,score+((card-computerSet[cnt])<0?card+computerSet[cnt]:-card-computerSet[cnt]),cnt+1);
+            comb-=cardBit;
+        }
+    }
+
+
+    	 //Fast IO
+    private static final int MAX_BUFFER_SIZE = 2; 
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    private static void initFI(){
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[MAX_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    private static int nextInt() throws IOException{
+        int ret = 0;
+        byte c = read();
+        while(c <= ' ') c= read();
+        boolean neg = (c == '-');
+        if(neg) c = read();
+        do{
+            ret = ret*10+c-'0';
+        }while((c=read())>='0' && c<='9');
+        if(neg) return (int) -ret;
+        return (int) ret;
+    }
+    
+    private static char nextChar() throws IOException{
+        byte c = read();
+        while(c <= ' ') c= read();
+        return (char)c;
+    }
+    
+    private static byte read() throws IOException{
+        if(curIdx == maxIdx){
+            maxIdx = inputStream.read(buffer,curIdx = 0, MAX_BUFFER_SIZE);
+            if(maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
+}
