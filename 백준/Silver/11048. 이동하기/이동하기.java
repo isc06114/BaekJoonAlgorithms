09ed0bbd82e64b53b4595ec025625a46e9d838ff
isc06114 <88813412+isc06114@users.noreply.github.com>
2024-02-28 13:36:40 +0900

    [Silver II] Title: 이동하기, Time: 400 ms, Memory: 80384 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -9,7 +9,7 @@ public class Main {
     static StringTokenizer st;
     static int R,C, mapNum[];
     static int map[][],memo[][];
-    static int[] left = {0,-1},up = {-1,0},leftUp = {-1,-1},mappers[] = {left,up,leftUp};
+    static int[] left = {0,-1},up = {-1,0},mappers[] = {left,up};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
@@ -21,17 +21,9 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             for(int c = 0; c < C; c++){
                 map[r][c] = Integer.parseInt(st.nextToken());
-            }
-        }
-        for(int r = 0; r < R; r++){
-            for(int c = 0; c < C; c++){
                 int temp = 0;
-                for(int[]mapper:mappers){
-                    int rr = r+mapper[0];
-                    int cc = c+mapper[1];
-                    if(rr<0||cc<0) continue;
-                    temp = Math.max(temp,memo[rr][cc]);
-                }
+                if(r>0) temp = Math.max(temp,memo[r-1][c]);
+                if(c>0) temp = Math.max(temp,memo[r][c-1]);
                 memo[r][c] = temp+map[r][c];
             }
         }
